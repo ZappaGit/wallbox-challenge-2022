6ef79324a1be5db19b6d36d10f4cd62bb628055d
zappagit <dgizquierdo@gmail.com>
2022-06-05 19:12:00 +0200

    working post with api

--- tests/config.js
+++ tests/config.js
@@ -3,4 +3,33 @@ const path = require("path");
 module.exports = {
   baseUrl: "http://localhost:3000",
   db: path.join(__dirname, "./db/db.json"),
+  user_ok_1: {
+    emailConfirmation: "mateo@mail.com",
+    email: "mateo@mail.com",
+    password: "12345678",
+    passwordConfirmation: "12345678",
+    role: "user",
+  },
+  user_ok_2: {
+    emailConfirmation: "mateo@mail.com",
+    email: "mateo@mail.com",
+    password: "12345678",
+    passwordConfirmation: "12345678",
+    role: "user",
+  },
+  user_without_confirms: {
+    email: "mateo@mail.com",
+    password: "12345678",
+    role: "user",
+  },
+  user_without_requiredData: {
+    email: "mateo@mail.com",
+  },
+  user_without_passwordOK: {
+    emailConfirmation: "mateo@mail.com",
+    email: "mateo@mail.com",
+    password: "123456",
+    passwordConfirmation: "123456",
+    role: "user",
+  },
 };
